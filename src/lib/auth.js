8f6e3e93645c3deb5830aa89821058ebbe4a7448
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2023-02-13 19:22:59 +0530

    Login page upadate

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,7 @@
-import { Auth } from '@zalter/identity-js';
+const auth = () => {
+  const token = localStorage.getItem("access-key");
 
-const noop = () => {};
-
-export const ENABLE_AUTH = process.env.NEXT_PUBLIC_ENABLE_ZALTER_AUTH === 'true';
-
-export const auth = ENABLE_AUTH
-  ? new Auth({
-    projectId: process.env.NEXT_PUBLIC_ZALTER_PROJECT_ID
-  })
-  : noop();
+  if (!token) {
+    res.redirect("/login");
+  }
+};
